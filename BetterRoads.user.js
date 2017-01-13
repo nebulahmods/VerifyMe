297481d1f7ca8ba4f456a9c517d138bfe5a18050
nebulahmods <kxnebulah@gmail.com>
2017-01-12 16:43:35 -0800

    Rename NewRoads.user.js to BetterRoads.user.js

---
+++ BetterRoads.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         BetterRoads
+// @version      v002
+// @description  make room for improvement while this keeps it
+// @author       New Roads School
+// @include        https://newroads.myschoolapp.com/app/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.location = "http://myscchoolapp.dx.am";
+})();
